f393fea6981d4c858fae5d96c3ff7dd5e1fbc131
Mohammed Harish <mohamharish0007@gmail.com>
2026-07-14 14:39:45 +0530

    basic python program added

---
+++ list/36_binary_search.py
@@ -0,0 +1,14 @@
+# 36. Binary Search
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+print(binary_search([1, 3, 5, 7, 9, 11], 7))
